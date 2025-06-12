7a9e4de054e9adc5295c1e293757c93b8a578e58
Nashir Resta <nashirresta7@gmail.com>
2025-06-12 07:56:43 +0700

    fix: path aliases

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import express, {Request, Response, type Express} from 'express';
-import {msg} from '@/api/test';
+import {env} from './common/utils/envConfig';
+import {app, logger} from './server';
 
-const app: Express = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send(msg);
+const server = app.listen(env.PORT, () => {
+  const {NODE_ENV, HOST, PORT} = env;
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
 });
 
-export default app;
+const onCloseSignal = () => {
+  logger.info('sigint received, shutting down');
+  server.close(() => {
+    logger.info('server closed');
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+};
+
+process.on('SIGINT', onCloseSignal);
+process.on('SIGTERM', onCloseSignal);
