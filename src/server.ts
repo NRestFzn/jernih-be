f3e1ab05cbf648faa2612727158f4560b6ff9f1c
Nashir Resta <nashirresta7@gmail.com>
2025-06-13 10:12:17 +0700

    fix: type

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,37 @@
-import express, {Request, Response} from 'express';
+import cors from 'cors';
+import express, {Request, Response, type Express} from 'express';
+import helmet from 'helmet';
+import {pino} from 'pino';
 
-const app = express();
+import errorHandler from './common/middleware/errorHandler';
+import rateLimiter from './common/middleware/rateLimiter';
+import requestLogger from './common/middleware/requestLogger';
+import {env} from './common/utils/envConfig';
+import indexRouter from './routes/index';
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('welcome');
-});
+const logger = pino({name: 'server start'});
+const app: Express = express();
 
-export default app;
+// Set the application to trust the reverse proxy
+app.set('trust proxy', true);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors({origin: env.CORS_ORIGIN, credentials: true}));
+app.use(helmet());
+app.use(rateLimiter);
+
+// Request logging
+app.use(requestLogger);
+
+// Routes
+// app.use('/health-check', healthCheckRouter);
+// app.use('/users', userRouter);
+// app.use('/auth', authRouter);
+app.use(indexRouter);
+
+// Error handlers
+app.use(errorHandler());
+
+export {app, logger};
