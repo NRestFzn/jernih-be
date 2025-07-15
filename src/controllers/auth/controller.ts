fccc8c6c63fc7989be9e6db4be8aa11156974f97
Nashir Resta <nashirresta7@gmail.com>
2025-07-15 10:17:33 +0700

    fix: path aliases

--- src/controllers/auth/controller.ts
+++ src/controllers/auth/controller.ts
@@ -1,19 +1,26 @@
 import {Request, Response} from 'express';
-import routes from '../../routes/v1';
+import routes from '@/routes/v1';
 import authService from './service';
+import asyncHandler from '@/modules/AsyncHandler';
 
-routes.post('/auth/signin', async (req: Request, res: Response) => {
-  const formData = req.body;
+routes.post(
+  '/auth/signin',
+  asyncHandler(async (req: Request, res: Response) => {
+    const formData = req.body;
 
-  const serviceResponse = await authService.signIn(formData);
+    const serviceResponse = await authService.signIn(formData);
 
-  res.status(serviceResponse.statusCode).json(serviceResponse);
-});
+    res.status(serviceResponse.statusCode).json(serviceResponse);
+  })
+);
 
-routes.post('/auth/signup', async (req: Request, res: Response) => {
-  const formData = req.body;
+routes.post(
+  '/auth/signup',
+  asyncHandler(async (req: Request, res: Response) => {
+    const formData = req.body;
 
-  const serviceResponse = await authService.signUp(formData);
+    const serviceResponse = await authService.signUp(formData);
 
-  res.status(serviceResponse.statusCode).json(serviceResponse);
-});
+    res.status(serviceResponse.statusCode).json(serviceResponse);
+  })
+);
