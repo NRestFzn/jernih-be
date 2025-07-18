d57388c0757bfd95da7adc15792b352b57fa5b6b
Nashir Resta <nashirresta7@gmail.com>
2025-07-18 10:22:27 +0700

    feat: add routes

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -5,3 +5,5 @@ const routes: Router = express.Router();
 export default routes;
 require('../controllers/auth/controller');
 require('../controllers/posts/controller');
+require('../controllers/techstack/controller');
+require('../controllers/experience/controller');
